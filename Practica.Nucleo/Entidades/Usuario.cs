9d4ab79d3355bf5cc428a4a499c93f34f7b131e9
Abrilrguez <33942729+Abrilrguez@users.noreply.github.com>
2018-11-23 10:33:13 -0700

    Se cambio configuracion de webconfig

--- Practica.Nucleo/Entidades/Usuario.cs
+++ Practica.Nucleo/Entidades/Usuario.cs
@@ -56,7 +56,8 @@ namespace Practica.Nucleo.Entidades
             using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
             {
                 byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(Hash));
-                using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider()
+                { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
                 {
                     ICryptoTransform transform = tripDes.CreateEncryptor();
                     byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
