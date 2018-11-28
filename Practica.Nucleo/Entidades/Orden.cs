9bded51f49a80981e8d5284fb6f8702e9d40088b
Abrilrguez <33942729+Abrilrguez@users.noreply.github.com>
2018-11-27 18:31:35 -0700

    Se cambio un atributo en la orden.

--- Practica.Nucleo/Entidades/Orden.cs
+++ Practica.Nucleo/Entidades/Orden.cs
@@ -170,7 +170,7 @@ namespace Practica.Nucleo.Entidades
             }
             return o;
         }
-        public static bool Guardar(int idOrden, int ordenEstado, double ordenPrecio, string ordenFolio, string ordenNumRastreo, string ordenFecha,
+        public static bool Guardar(int idOrden, int ordenEstado, double ordenPrecio, string ordenFolio, string ordenNumRastreo, DateTime ordenFecha,
                                     int idUsuario,
                                     int idPaquete, string paquetePeso, string paqueteTamanio, string paqueteContenido, string paqueteDescripcion,
                                     int idCliente, string clienteNombre, string clienteTelefono, string clienteCorreo, string clienteRfc, string clienteDomicilio,
