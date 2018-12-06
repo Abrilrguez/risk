13e2e95c69581603e5a7a8bef272bb10e4a750af
Valeria García Cobian <valeriacobianisw@gmail.com>
2018-12-05 18:24:12 -0700

    Se cambió script de orden.

--- Web.Aplicacion/Scripts/rastreo.js
+++ Web.Aplicacion/Scripts/rastreo.js
@@ -1,15 +1,1 @@
-﻿function rastrear()
-{
-    var id = $('#numero-rastreo').val();
-    alert(id);
-    $.ajax({
-        url: baseUrl + "Orden/ObtenerPorFolio/"+id,
-        cache: false,
-        contentType: "application/json; charset=utf-8",
-        success: function (data) {
-            
-            var modalC = $("#info");
-            $('#info').modal();
-        }
-    });
-}
+﻿
